9ed29fb19f8a92976f98bd513a2f8e6e12760b7d
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-09 08:50:58 +0000

    Added menu method

--- lib/chart_finder/cli.rb
+++ lib/chart_finder/cli.rb
@@ -3,6 +3,7 @@ class ChartFinder::CLI
   def call 
     puts "testing setup"
     lists_songs
+    menu
   end
   
   
@@ -11,6 +12,10 @@ class ChartFinder::CLI
     1. first song - artist - date
     2. second song - artist - date
  DOC
+ end
+ 
+ def menu
+   puts "Enter the number of the song that you would like more information about"
  end
 
 end
