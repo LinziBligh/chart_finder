91e7712f74c1182fd8f7661c91d1add706ac6059
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-09 09:02:25 +0000

    Added request for date to search in call method

--- lib/chart_finder/cli.rb
+++ lib/chart_finder/cli.rb
@@ -1,13 +1,16 @@
 class ChartFinder::CLI
   
   def call 
-    puts "testing setup"
+    puts "What is the date of the chart that you would like to look up?"
+    @date = gets.strip
     lists_songs
     menu
+    
   end
   
   
   def lists_songs
+    puts "Here is the offial UK chart for #{@date}"
     puts <<-DOC #look up "here doc" ...
     1. first song - artist - date
     2. second song - artist - date
