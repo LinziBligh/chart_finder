1da6009ede4ba2e378cf2b86c85d26d4efd51b8b
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-09 19:20:24 +0000

    added attr_accessor for name artist label and coverlink

--- lib/chart_finder/song.rb
+++ lib/chart_finder/song.rb
@@ -1,11 +1,26 @@
 class ChartFinder:: Song 
+  attr_accessor :name, :artist, :label, :coverlink
 
 def self.chart
- 
-    puts <<-DOC #look up "here doc" ...
+  puts <<-DOC #look up "here doc" ...
     1. first song - artist - date
     2. second song - artist - date
  DOC
+ song1 = self.new
+ song1.name = "Pipes of Peace"
+ song1.artist = "Paul Mcartney"
+ song1.label = "Parlaphone"
+ song1.coverlink = "www.coverlink.com"
+ 
+ song2 = self.new
+ song2.name = "Vengabus is coming"
+ song2.artist = "Vengboys"
+ song2.label = "Eurotrash"
+ song2.coverlink = "www.coverlink.com"
+ 
+ [song1, song2]
+ 
+ 
   #should return all of the instances of songs in the chart from this day.
 end
 
