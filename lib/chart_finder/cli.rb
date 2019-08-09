66004c0f741bc00bd3cda44a8f88b5e6e136cacb
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-09 13:48:43 +0000

    added chart option to menu, renamed list_songs

--- lib/chart_finder/cli.rb
+++ lib/chart_finder/cli.rb
@@ -3,14 +3,14 @@ class ChartFinder::CLI
   def call 
     puts "What is the date of the chart that you would like to look up?"
     @date = gets.strip
-    lists_songs
+    list_songs
     menu
     goodbye
     
   end
   
   
-  def lists_songs
+  def list_songs
     puts "Here is the offial UK chart for #{@date}"
     puts <<-DOC #look up "here doc" ...
     1. first song - artist - date
@@ -22,9 +22,11 @@ class ChartFinder::CLI
   
    input = nil
    while input != "exit"
-    puts "Enter the number of the song that you would like more information about, or type exit"
+    puts "Enter the number of the song that you would like more information about, type chart to see the chart again or type exit to end"
    input = gets.strip.downcase
       case input
+        when "chart"
+          list_songs
         when "1"
         puts "More info about song 1"
         when "2"
