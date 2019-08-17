f91b72f0e73f001f1095d7081a81693ffce45d90
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-17 11:47:04 +0000

    link to coverart works, but is tiny image

--- lib/chart_finder/song.rb
+++ lib/chart_finder/song.rb
@@ -12,11 +12,9 @@ def self.scrape(url)
     new_hash[:title] = song.css(".title a").text.split.map(&:capitalize).join(' ')
     new_hash[:artist] = song.css(".artist a").text.split.map(&:capitalize).join(' ')
     new_hash[:label] = song.css(".label").text.split.map(&:capitalize).join(' ')
-   new_hash[:coverlink] = song.css(".cover").children.attribute("src")
+   new_hash[:coverlink] = song.css(".cover img").attribute("src").value
    
-   #"https://images-eu.ssl-images-amazon.com/images/I/619fzjO1rmL.jpg" 
     song_array << new_hash
-    binding.pry
 end
 song_array
 end
