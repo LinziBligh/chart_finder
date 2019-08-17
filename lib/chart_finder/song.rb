5babcc6948356324b3b8d1b55ca1bab6e91c9f19
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-17 10:40:56 +0000

    added capitalize methods to scraper

--- lib/chart_finder/song.rb
+++ lib/chart_finder/song.rb
@@ -8,9 +8,9 @@ def self.scrape(url)
   song_array = []
   songs.each do |song|
     new_hash = {}
-    new_hash[:title] = song.css(".title a").text
-    new_hash[:artist] = song.css(".artist a").text
-    new_hash[:label] = song.css(".label").text
+    new_hash[:title] = song.css(".title a").text.split.map(&:capitalize).join(' ')
+    new_hash[:artist] = song.css(".artist a").text.split.map(&:capitalize).join(' ')
+    new_hash[:label] = song.css(".label").text.split.map(&:capitalize).join(' ')
    new_hash[:coverlink] = "https://images-eu.ssl-images-amazon.com/images/I/619fzjO1rmL.jpg" #song.css(".cover").children.attribute("src").value #not correct
     song_array << new_hash
 end
