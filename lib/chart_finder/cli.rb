19ae7b7e2dcc5a75629f4b16cca341153611a621
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-18 17:05:42 +0000

    adjusted spacing

--- lib/chart_finder/cli.rb
+++ lib/chart_finder/cli.rb
@@ -43,22 +43,12 @@ class ChartFinder::CLI
     
     ChartFinder::Song.all.each.with_index do |song, index|
       puts "#{index + 1}. #{song.title} - #{song.artist} "
-  end
+    end
+    puts "****************************************************************"
+    puts ""
 end
   
-  # def display_students
-  #   Student.all.each do |student|
-  #     puts "#{student.name.upcase}".colorize(:blue)
-  #     puts "  location:".colorize(:light_blue) + " #{student.location}"
-  #     puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
-  #     puts "  bio:".colorize(:light_blue) + " #{student.bio}"
-  #     puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
-  #     puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
-  #     puts "  github:".colorize(:light_blue) + " #{student.github}"
-  #     puts "  blog:".colorize(:light_blue) + " #{student.blog}"
-  #     puts "----------------------".colorize(:green)
-  #   end
-  # end
+  
  
  
   def menu
